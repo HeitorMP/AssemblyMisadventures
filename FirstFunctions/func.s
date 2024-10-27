59e757c620f3855bdacf894dbbbedb966ca7e58e
heitormp <heitormp@protonmail.com>
2024-10-27 20:40:14 +0000

    string length

--- FirstFunctions/func.s
+++ FirstFunctions/func.s
@@ -6,6 +6,7 @@ section .text                         	; Define the code section
 global thisFunctionOnlyReturns42        ; Makes the function visible externally, prototype
 global get_message                      
 global get_numbers
+global get_string_length 
 
 thisFunctionOnlyReturns42:              ; Define the label for the function
     mov rax, 42                         ; Move the value 42 into the RAX register (return value)
@@ -18,3 +19,17 @@ get_message:                            ; Define a function that returns the str
 get_numbers:                            ; Define a function that returns the array
     mov rax, numbers                    ; Move the address of the numbers array into RAX
     ret                                 ; Return from the function
+
+get_string_length:
+    mov rdi, string                     ; Move string to rdi
+    xor rax, rax                        ; 0 rax value
+
+.get_string_length_loop:                ; this loop for strng length
+    cmp byte [rdi + rax], 0             ; Compare rdi(byte position of string) + rax(index) with 0(null terminator)
+    je .get_string_length_end_loop      ; je = jump if equal. If cpm = 0 break the loop
+    inc rax                             ; if cmp != 0 rax++
+    jmp .get_string_length_loop         ; loop
+
+.get_string_length_end_loop:
+    ret                                 ; Return from the function
+
--- FirstFunctions/main.cpp
+++ FirstFunctions/main.cpp
@@ -3,6 +3,7 @@
 extern "C" int		thisFunctionOnlyReturns42();
 extern "C" char*	get_message();
 extern "C" int*		get_numbers();
+extern "C" int		get_string_length();
 
 int main() {
 	std::cout << "thisFunctionOnlyReturns42: " << thisFunctionOnlyReturns42() << std::endl;
@@ -15,4 +16,10 @@ int main() {
 	for (int i = 0; i < 5; i++) {
 		std::cout << "numbers[" << i << "]: " << numbers[i] << std::endl;
 	}
+
+	char *my_string = get_message();
+	std::cout << "message again: " << my_string << std::endl;
+	std::cout << "message size: " << get_string_length() << std::endl;
+
+	exit (0);
 }
